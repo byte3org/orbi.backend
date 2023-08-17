dd0a067bfe0666403da8f46ec3f1621d76de31c7
rxOred <rxored@gmail.com>
2023-08-17 09:22:13 +0530

    index

---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+
+const express = require("express");
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 4444;
+
+// import routes
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', '*');
+
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'POST', 'DELETE', 'GET');
+		return res.status(200).json({});
+	}
+	next();
+})
+
+// routes
+
+app.use((req, res, next) => {
+	const error = new Error('NotFound');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		"error": {
+			message: error.message
+		}
+	});
+});
+
+app.listen(port, () => {
+	console.log("listening on port ", port)
+})
