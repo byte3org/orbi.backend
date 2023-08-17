f376a07d1be9ba1e32c4dd2bee8be85e8e6d4bdc
rxOred <rxored@gmail.com>
2023-08-17 09:23:21 +0530

    routes

---
+++ api/routes/reviews.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const reviewController = require('../controllers/review');
+
+router.get('/', )
+
+router.get('/:id')
+
+router.post('/')
+
+
